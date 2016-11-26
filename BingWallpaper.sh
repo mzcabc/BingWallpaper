4ef5863708abf70546520e13068d6df843349b88
Mindy Cong <mindycong@gmail.com>
2016-11-26 14:33:49 +0800

    change dir && add cron job

--- BingWallpaper.sh
+++ BingWallpaper.sh
@@ -2,6 +2,6 @@
 baseurl="www.bing.com"
 imgurl=$(expr "$(curl -L $baseurl/?mkt=zh-CN | grep hprichbg)" : '.*g_img={url: "\(.*\)",id.*')
 filename=$(expr "$imgurl" : '.*/\(.*\)')
-localpath="/Users/$USER/Pictures/Bing/$(date "+%Y-%m-%d")-$filename"
+localpath="/Users/$USER/Pictures/BingWallpaper/$(date "+%Y-%m-%d")-$filename"
 curl -o $localpath  $baseurl/$imgurl
 osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$localpath\""
