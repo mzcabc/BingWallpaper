1197431b2184a02f94f538b8045138f9b9c54786
Mindy Cong <mindycong@gmail.com>
2016-11-26 15:17:47 +0800

    Determine local pic exist

--- BingWallpaper.sh
+++ BingWallpaper.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
-baseUrl="cn.bing.com"
 localDir="/Users/$USER/Pictures/BingWallpaper"
-imgurl=$(expr "$(curl -L $baseUrl | grep hprichbg)" : '.*hprichbg\(.*\)",id.*')
-filename=$(expr "$imgurl" : '.*/\(.*\)')
-localpath="$localDir/$(date "+%Y-%m-%d")-$filename"
-curl -o $localpath  $baseUrl/az/hprichbg/$imgurl
-osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$localpath\""
+filenameRegex=".*"$(date "+%Y-%m-%d")".*jpg"
+log=$localDir/log.log
+
+findResult=$(find $localDir -regex $filenameRegex)
+if [ ! -n "$findResult" ]; then
+    baseUrl="cn.bing.com"
+    imgurl=$(expr "$(curl -L $baseUrl | grep hprichbg)" : '.*hprichbg\(.*\)",id.*')
+    filename=$(expr "$imgurl" : '.*/\(.*\)')
+    localpath="$localDir/$(date "+%Y-%m-%d")-$filename"
+    curl -o $localpath $baseUrl/az/hprichbg/$imgurl
+    osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$localpath\""
+    echo "$(date +"%Y-%m-%d %H:%M:%S") Downloaded $filename" >> log
+else
+    echo "$(date +"%Y-%m-%d %H:%M:%S") Exist" >> log
+    exit 0
+fi
