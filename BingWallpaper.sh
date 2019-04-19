94768f41bbc996686f1f84a6e5575d0adb60ef77
Mindy Cong <mindycong@gmail.com>
2019-04-19 15:20:38 +0800

    add pic description

--- BingWallpaper.sh
+++ BingWallpaper.sh
@@ -11,17 +11,22 @@ findResult=$(find $localDir -regex $filenameRegex)
 
 if [ ! -n "$findResult" ]; then
     baseUrl="cn.bing.com"
-    imgurl=$(expr "$(curl -L $baseUrl | grep "&amp;rf")" : '.*href=\"\(\/th\?id=OHR\.[A-Za-z]*\_ZH\-CN[0-9]*\_1920x1080\.jpg\).*')
+    html=$(curl -L $baseUrl)
+
+    imgurl=$(expr "$(echo "$html" | grep "&amp;rf")" : '.*href=\"\(\/th\?id=OHR\.[A-Za-z]*\_ZH\-CN[0-9]*\_1920x1080\.jpg\).*')
     echo $imgurl
     filename=$(expr "$imgurl" : '.*OHR\.\(.*\)')
     echo $filename
     localpath="$localDir/$(date "+%Y-%m-%d")-$filename"
     curl -o $localpath $baseUrl/$imgurl
+
+    des=$(expr "$(echo "$html" | grep "id=\"sh_cp\" class=\"sc_light\"")" : '.*id=\"sh_cp\".*title=\"\(.*\)\" aria-label=\"主页图片信息\"')
+
     osascript -e "                              \
         tell application \"System Events\" to   \
             tell every desktop to               \
                 set picture to \"$localpath\""
-    osascript -e "display notification \"$filename Downloaded\" with title \"BingWallpaper\""
+    osascript -e "display notification \"$des\" with title \"BingWallpaper\""
     echo "$(date +"%Y-%m-%d %H:%M:%S") Downloaded $filename" >> $log
 else
     echo "$(date +"%Y-%m-%d %H:%M:%S") Exist" >> $log
